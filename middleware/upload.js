c105670180ca1ee374b3e178e8d88a625abfc281
Misbah <misbah786sk@gmail.com>
2026-04-14 21:10:28 +0530

    please please please work

--- middleware/upload.js
+++ middleware/upload.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Store temporarily on server
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // make sure this folder exists
+    cb(null, "/tmp"); // make sure this folder exists
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
