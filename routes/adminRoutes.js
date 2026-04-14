2bbc55099e357b7f3fb4961956de461e2353a53b
Misbah <misbah786sk@gmail.com>
2026-04-14 21:21:56 +0530

    one more attempt

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -153,8 +153,8 @@ async function generateInvoicePDF(payment) {
   `;
 
   const browser = await puppeteer.launch({
-    headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    headless: true,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
   const page = await browser.newPage();
